267246c13bc32d7047cf0e548be01dd15dbc0fdd
Thomas Gray <tgray017@gmail.com>
2019-04-24 13:10:10 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,6 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require_all 'app'
+
+connection_details = YAML::load(File.open('config/database.yml'))
+ActiveRecord::Base.establish_connection(connection_details)
